9e1bca04ec89002d3c11b37d169ef477d663fdc7
Alex <alexrkoch@gmail.com>
2021-07-28 12:24:58 -0400

    add win loss checkers and field modifiers

--- main.js
+++ main.js
@@ -8,48 +8,73 @@ const pathCharacter = "*";
 class Field {
   constructor(field, verticalLimit, horizontalLimit) {
     this.field = field;
-    this.verticalPosition = 1;
-    this.horizontalPosition = 1;
+    this.verticalPosition = 0;
+    this.horizontalPosition = 0;
     this.verticalLimit = verticalLimit;
     this.horizontalLimit = horizontalLimit;
+    this.gameStatus = "active";
   }
   static generateField() {}
   printField() {
     this.field.forEach((element) => console.log(element.join("")));
   }
   handleInput() {
-    // console.log("initialVerticalPosition: " + this.verticalPosition);
-    // console.log("initialHorizontalPosition: " + this.horizontalPosition);
+    // consider adding losing if you go off board.
     let input = prompt("What's your next move? ");
-    if ((input === "u") & (this.verticalPosition > 1)) {
+    if ((input === "u") & (this.verticalPosition > 0)) {
       this.verticalPosition--;
     } else if (
       (input === "r") &
-      (this.horizontalPosition < this.horizontalLimit)
+      (this.horizontalPosition < this.horizontalLimit - 1)
     ) {
       this.horizontalPosition++;
-    } else if (input === "d") {
+    } else if (
+      (input === "d") &
+      (this.verticalPosition < this.verticalLimit - 1)
+    ) {
       this.verticalPosition++;
-    } else if ((input === "l") & (this.horizontalPosition > 1)) {
+    } else if ((input === "l") & (this.horizontalPosition > 0)) {
       this.horizontalPosition--;
     }
-    // console.log("newVerticalPosition: " + this.verticalPosition);
-    // console.log("newHorizontalPosition: " + this.horizontalPosition);
   }
-  checkForInBounds() {}
-  checkForWin() {}
-  checkForLoss() {}
-  modifyField() {}
-  gameFlow() {
-    this.handleInput();
-    this.checkForInBounds();
+  checkForWin() {
+    if (this.field[this.verticalPosition][this.horizontalPosition] === hat) {
+      console.log("You found the hat - You win!!");
+    }
+    this.gameStatus = "over";
+  }
+  checkForLoss() {
+    if (this.field[this.verticalPosition][this.horizontalPosition] === hole) {
+      console.log("Whoops you fell in a hole :(");
+    }
+    this.gameStatus = "over";
+  }
+  modifyField() {
+    if (this.gameStatus === "active") {
+      this.field[this.verticalPosition][this.horizontalPosition] = "*";
+    }
+  }
+  playGame() {
+    while (this.gameStatus === "active") {
+      this.printField();
+      this.handleInput();
+      this.checkForWin();
+      this.checkForLoss();
+      this.modifyField();
+      this.printField();
+    }
   }
 }
 
-// Your class should take a single argument representing the field:
+// const newField = Field.generateField(x, y);
+// const myField = new Field(newField);
+// myField.gamePlay();
+
+// TESTING PLAYGROUND
+
 const myField = new Field(
   [
-    ["*", "░", "O"],
+    ["*", "^", "O"],
     ["░", "O", "░"],
     ["░", "^", "░"],
   ],
@@ -57,4 +82,4 @@ const myField = new Field(
   3
 );
 
-myField.handleInput();
+myField.playGame();
