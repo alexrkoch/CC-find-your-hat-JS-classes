89111ac0aa0fb7d93e986645513db70e4dd07f5d
Alex <alexrkoch@gmail.com>
2021-07-28 11:43:12 -0400

    implement tested handleInput() function

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const pathCharacter = "*";
 class Field {
   constructor(field, verticalLimit, horizontalLimit) {
     this.field = field;
-    this.verticalPosition = 0;
-    this.horizontalPosition = 0;
+    this.verticalPosition = 1;
+    this.horizontalPosition = 1;
     this.verticalLimit = verticalLimit;
     this.horizontalLimit = horizontalLimit;
   }
@@ -18,16 +18,23 @@ class Field {
     this.field.forEach((element) => console.log(element.join("")));
   }
   handleInput() {
+    // console.log("initialVerticalPosition: " + this.verticalPosition);
+    // console.log("initialHorizontalPosition: " + this.horizontalPosition);
     let input = prompt("What's your next move? ");
-    if ((input === "u") & (this.verticalPosition > 0)) {
+    if ((input === "u") & (this.verticalPosition > 1)) {
       this.verticalPosition--;
-    } else if (input === "r") {
+    } else if (
+      (input === "r") &
+      (this.horizontalPosition < this.horizontalLimit)
+    ) {
       this.horizontalPosition++;
     } else if (input === "d") {
       this.verticalPosition++;
-    } else if (input === "l") {
-      this.horizontalPosition++;
+    } else if ((input === "l") & (this.horizontalPosition > 1)) {
+      this.horizontalPosition--;
     }
+    // console.log("newVerticalPosition: " + this.verticalPosition);
+    // console.log("newHorizontalPosition: " + this.horizontalPosition);
   }
   checkForInBounds() {}
   checkForWin() {}
@@ -40,8 +47,14 @@ class Field {
 }
 
 // Your class should take a single argument representing the field:
-const myField = new Field([
-  ["*", "░", "O"],
-  ["░", "O", "░"],
-  ["░", "^", "░"],
-]);
+const myField = new Field(
+  [
+    ["*", "░", "O"],
+    ["░", "O", "░"],
+    ["░", "^", "░"],
+  ],
+  3,
+  3
+);
+
+myField.handleInput();
